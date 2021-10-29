6039527734a4cd51b43ce361f2a1ae3692d98b53
Clems-web <clement.servais1@gmail.com>
2021-10-29 10:31:16 +0200

    test error

--- public/index.php
+++ public/index.php
@@ -7,7 +7,9 @@ use Cleme\CdaStockLien\Controller\LinkController;
 
 require '..\vendor\autoload.php';
 
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 session_start();
